de76d195a938fef487a887eb906ec8a06df8867a
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-10-14 00:30:14 +0300

    update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+buttonEl.addEventListener('click', onChangeBodyColorClick);
+
+function onChangeBodyColorClick(event) {
+  const bodyColor = getRandomHexColor();
+  spanEl.textContent = bodyColor;
+  bodyEl.style.backgroundColor = bodyColor;
+}
