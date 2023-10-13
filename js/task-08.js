0e93df096247e2f426e1532a454cecb4bc7f79bb
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-10-14 00:23:58 +0300

    update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = { email, password };
+
+  if (email === '' || password === '') {
+    return alert('Attention! All fields must be completed');
+  }
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
+
+
+
 
